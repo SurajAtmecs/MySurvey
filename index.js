a1d89d0c04b16193f4854b6ee22b987e31802f96
Suraj <spatil1@follett.com>
2022-02-25 12:48:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export class MySurvey extends React.Component {
+  render() {
+    <View>
+      <Text> This is MySurvey.</Text>
+    </View>;
+  }
+}
