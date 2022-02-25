5fb5621cfc8d2367b6035f7baed1e3ce669347a0
Suraj <spatil1@follett.com>
2022-02-25 13:06:54 +0530

    Added button to component

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { View, Text, AppRegistry } from "react-native";
+import { View, Text, AppRegistry, Button, Alert } from "react-native";
 
 export class MySurvey extends React.Component {
   render() {
     return (
       <View>
         <Text> This is MySurvey.</Text>
+        <Button
+          title="Click here"
+          onPress={() => {
+            Alert.alert("You clicked the button");
+          }}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: "red",
+    height: 200,
+    width: 200,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default AppRegistry.registerComponent("MySurvey", MySurvey);
